0ef9e8f0d4eabccc7fe37b138fc6dda83a41ec09
321-dev <art5ee321@gmail.com>
2026-03-15 12:47:37 -0400

    Add files via upload

---
+++ sw1.js
@@ -0,0 +1,28 @@
+const CACHE = 'clarity-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Sora:wght@300;400;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
